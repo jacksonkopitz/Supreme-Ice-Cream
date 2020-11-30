9a5fd26fe779453c5f0633e5658d6f5727c71dc8
Jacob Mitrani <jcm497@cornell.edu>
2020-11-30 14:36:30 -0500

    working on login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import { useForm } from 'react-hook-form';
 
 
-const Login = () => {
-  return <div>
-    login here
-    </div>;
+const Login= () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let correctUser = false;
+  
+  function handleSubmit() {
+    if (email === "j@j.com" && password==="123") {
+      correctUser = true;
+    }
+    else {
+      alert('wrong email');
+    }
+  } 
+
+  if (!correctUser){
+    return (
+      <div>
+        <form onSubmit = {handleSubmit}>
+          <label>
+            Email:
+            <input type="email" value = {email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            <input type="password" value = {password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <input type="submit" value = "Submit" />
+        </form>
+      </div>
+    );
+
+  }
+  else {
+    return (
+     <div>
+       <p>correct user</p>
+       {/* create form for the user to add info to main page */}
+     </div> 
+    )
+  }
 }
 
 export default Login;
